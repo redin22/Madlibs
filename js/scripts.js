9b422cb4c9b4db3488bdc53ae316467fcf085436
Rehan Edin <rehanedin@gmail.com>
2018-04-30 11:47:59 +0300

    make changes

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+   $("#blanks form").submit(function(event) {
+     var blanks = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
+
+     blanks.forEach(function(blank) {
+     var userInput = $("input." + blank).val();
+       debugger;
+
+       $("." + blank).text(userInput).val();
+     });
+
+     $("#story").show();
+
+     event.preventDefault();
+   });
+ });
